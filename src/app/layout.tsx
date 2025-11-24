3f9e1a20ad076ce59c8c5f47d9aa66d569f563ec
Ben Avery <spacebaboon@gmail.com>
2025-11-24 15:55:51 +0100

    Add Vercel performance tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
     title: "RSS Aggregator",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {children}
+                <SpeedInsights />
+            </body>
         </html>
     );
 }
